1d53c10d46d641ba3ac35065205bb405ff6edcdc
Frangeris Peguero <frangeris.20@gmail.com>
2015-04-17 21:15:18 -0400

    Adding support for command line app

---
+++ src/skeleton/sdk/console/command/GenerateCommand.php
@@ -0,0 +1,23 @@
+<?php namespace Skeleton\SDK\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('provider:generate')
+            ->setDescription('Generate new provider')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    	echo 'funca';
+    }
+}
